bb0660edc62dc85b7d6ab30e19690187d903dc9b
adv3ntur3ss <xox.svk@gmail.com>
2022-03-13 14:02:54 +0100

    commit

--- src/plugins/visivig/pages/page-home.vue
+++ src/plugins/visivig/pages/page-home.vue
@@ -1,11 +1,17 @@
 <template>
 <div>
     <layout-header/>
+    <r-hr/>
+    <openlabPosuva/>
+    <ktoStojiZaOpenlabom/>
+    <r-hr/>
+    <nasaVizia/>
+    <r-hr/>
+    <napisaliONas/>
     <imagesPredDakujeme/>
     <dakujemeZaPodporu/>
     <newsletter/>
     <layout-footer/>
-
 </div>
 </template>
 <script>
@@ -14,8 +20,13 @@ export default {
         'layout-header': () => import('../layout/layout-header.vue'),
         'layout-footer': () => import('../layout/layout-footer.vue'),
         'dakujemeZaPodporu': () => import('../reusables/reusables-universal/dakujemeZaPodporuSection/r-dakujemeZaPodporu.vue'),
-        'imagesPredDakujeme': () => import('../reusables/reusables-universal/imagesPredDakujemeSection/imagesPredDakujeme.vue'),
-        'newsletter': () => import('../reusables/reusables-universal/newsletterSection/newsletter.vue')
+        'imagesPredDakujeme': () => import('../reusables/reusables-universal/imagesPredDakujemeSection/r-imagesPredDakujeme.vue'),
+        'newsletter': () => import('../reusables/reusables-universal/newsletterSection/r-newsletter.vue'),
+        'napisaliONas': () => import('../reusables/reusables-home/napisaliONasSection/r-napisaliONas.vue'),
+        'r-hr': () => import('../reusables/reusables-universal/hrSection/r-hr.vue'),
+        'nasaVizia': () => import('../reusables/reusables-universal/nasaViziaSection/r-nasaVizia.vue'),
+        'ktoStojiZaOpenlabom': () => import('../reusables/reusables-universal/ktoStojiZaOpenlabomSection/r-ktoStojiZaOpenlabom.vue'),
+        'openlabPosuva': () => import('../reusables/reusables-universal/openlabPosuvaSection/r-openlabPosuva.vue')
     }
         
 }
---
+++ src/plugins/visivig/reusables/reusables-universal/openlabPosuvaSection/r-openlabPosuva.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container mb-70 pdt-60">
+    <h1 class="hlc">OpenLab posúva možnosti vzdelávania</h1>
+    <h4 class="center hlc">
+      V OpenLabe študentov učíme najnovšie technológie. Zároveň im dávame veľkú
+      slobodu, pričom ich vedieme k zodpovednosti a samostatnosti, učíme ich
+      princípy partnerského prístupu, pracovať v tíme, trénujeme ich schopnosti
+      sebareflexie a podnecujeme ich kreativitu. Takto študentov pripravujeme
+      osobnostne aj tech zručnosťami na veľké veci, ktoré môžu v budúcnosti
+      tvoriť.
+    </h4>
+    <div id="idcards" class="row">
+      <div class="col max25">
+        <div class="card">
+          <div class="card-c-1 card-body-number">
+            <p class="card-c-1 big-num">10</p>
+            <p class="card-c-1 subtitle">labov</p>
+          </div>
+        </div>
+      </div>
+      <div class="col max25">
+        <div class="card">
+          <div class="card-c-2 card-body-number">
+            <p class="card-c-2 big-num">150</p>
+            <p class="card-c-2 subtitle">študentov</p>
+          </div>
+        </div>
+      </div>
+      <div class="col max25">
+        <div class="card">
+          <div class="card-c-3 card-body-number">
+            <p class="card-c-3 big-num">9</p>
+            <p class="card-c-3 subtitle">LabMastrov</p>
+          </div>
+        </div>
+      </div>
+      <div class="col max25">
+        <div class="card">
+          <div class="card-c-4 card-body-number">
+            <p class="card-c-4 big-num">2</p>
+            <p class="card-c-4 subtitle">mestá</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
