86d17bc5e2bdd4eaae65c5199ae0b04bc42962b3
adv3ntur3ss <xox.svk@gmail.com>
2022-05-17 09:07:45 +0200

    neviem co robim 404

--- wp-content/themes/wp-bootstrap-starter/404.php
+++ wp-content/themes/wp-bootstrap-starter/404.php
@@ -9,13 +9,6 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area col-sm-12 col-lg-8">
-		<div id="main" class="site-main" role="main">
-
-
-		
-		</div><!-- #main -->
-	</section><!-- #primary -->
 
 <?php
 get_sidebar();
