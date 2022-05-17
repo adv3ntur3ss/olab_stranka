e0c19556b8a453635fa92098b92ff76c1dcf8537
adv3ntur3ss <xox.svk@gmail.com>
2022-05-17 09:06:51 +0200

    neviem co robim 404

--- wp-content/themes/wp-bootstrap-starter/404.php
+++ wp-content/themes/wp-bootstrap-starter/404.php
@@ -12,23 +12,8 @@ get_header(); ?>
 	<section id="primary" class="content-area col-sm-12 col-lg-8">
 		<div id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wp-bootstrap-starter' ); ?></h1>
-				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wp-bootstrap-starter' ); ?></p>
-
-					<?php
-						get_search_form();
-
-
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
+		
 		</div><!-- #main -->
 	</section><!-- #primary -->
 
