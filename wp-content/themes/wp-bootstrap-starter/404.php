772d0aa025123404f143e39b9a5857dfa85abf0b
adv3ntur3ss <xox.svk@gmail.com>
2022-05-17 19:58:47 +0200

    404 page

--- wp-content/themes/wp-bootstrap-starter/404.php
+++ wp-content/themes/wp-bootstrap-starter/404.php
@@ -8,15 +8,11 @@
  */
 
 get_header(); ?>
-<div class="container">
-	<div class="row">
-		<div id="main" class="site-main error-content" role="main">
-			<div class="page-content error-content-wrapper">
-				<h2>Stránka, ktorú hladáš neexistuje</h2>
-				<img src="wp-content/themes/wp-bootstrap-starter/assets/images/404_img.png">
-				<a href="/">HOME</a>
-			</div>
-		</div>
+<div id="main" class="site-main error-content" role="main">
+	<div class="page-content error-content-wrapper">
+		<h2>Stránka, ktorú hladáš neexistuje</h2>
+		<img src="wp-content/themes/wp-bootstrap-starter/assets/images/404_img.png">
+		<a href="/">HOME</a>
 	</div>
 </div>
 <?php
