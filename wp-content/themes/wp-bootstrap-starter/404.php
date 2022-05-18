cbf289e0414457c76308ef05f2759e492044112f
adv3ntur3ss <xox.svk@gmail.com>
2022-05-18 10:01:41 +0200

    404 page

--- wp-content/themes/wp-bootstrap-starter/404.php
+++ wp-content/themes/wp-bootstrap-starter/404.php
@@ -11,7 +11,7 @@ get_header(); ?>
 <div id="main" class="site-main error-content" role="main">
 	<div class="page-content error-content-wrapper">
 		<h2>Stránka, ktorú hladáš, neexistuje</h2>
-		<img src="wp-content/themes/wp-bootstrap-starter/assets/images/404_img.png">
+		<img src="https://hugo-openlab.wezeo.camp/wp-content/uploads/2022/05/404_img.png">
 		<a class="mb-70" href="/">HOME</a>
 	</div>
 </div>
