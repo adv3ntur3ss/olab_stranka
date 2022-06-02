ae211a9f13ef4aac48d261df86034b769147eb9c
adv3ntur3ss <xox.svk@gmail.com>
2022-06-02 15:34:21 +0200

    header fix

--- wp-content/themes/wp-bootstrap-starter/header.php
+++ wp-content/themes/wp-bootstrap-starter/header.php
@@ -48,7 +48,7 @@
               <img id="iqiom" src="/wp-content/themes/wp-bootstrap-starter/assets/images/5eb1d693b4315openlab-logo.png" />
             </a>
           </div>
-          <div><a <?php echo ($filename == '' || "home") ? 'class="active"' : ''; ?> class="links" href="/home">Úvod</a></div>
+          <div><a <?php echo ($filename == '') ? 'class="active"' : ''; ?> class="links" href="/">Úvod</a></div>
           <div><a <?php echo ($filename == 'openlaby') ? 'class="active"' : ''; ?> class="links" href="/openlaby">Openlaby</a></div>
           <div><a <?php echo ($filename == 'nas-tim') ? 'class="active"' : ''; ?> class="links" href="/nas-tim">Náš tím</a></div>
           <div><a <?php echo ($filename == 'novinky') ? 'class="active"' : ''; ?> class="links" href="/novinky">Novinky</a></div>
@@ -84,7 +84,7 @@
         </nav>
         <div class="collapse" id="navbarToggleExternalContent">
           <div class="navbar-divs px-4 pb-4">
-            <div><a <?php echo ($filename == '') ? 'class="active"' : ''; ?> class="links" href="/home">Úvod</a></div>
+            <div><a <?php echo ($filename == '') ? 'class="active"' : ''; ?> class="links" href="/">Úvod</a></div>
             <div><a <?php echo ($filename == 'openlaby') ? 'class="active"' : ''; ?> class="links" href="/openlaby">Openlaby</a></div>
             <div><a <?php echo ($filename == 'nas-tim') ? 'class="active"' : ''; ?> class="links" href="/nas-tim">Náš tím</a></div>
             <div><a <?php echo ($filename == 'novinky') ? 'class="active"' : ''; ?> class="links" href="/novinky">Novinky</a></div>
